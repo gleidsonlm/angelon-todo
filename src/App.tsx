bafe519c8ecf5b3209c740cb15e442ef9859dcae
GleidsonLM <gleidsonlm@gmail.com>
2023-01-30 14:58:32 -0300

    start: vite+react with typescript+swc. add: eslint/prettier

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+export function Header() {
+
+  return (
+    <>
+      <h1>AgenlOn ToDo&apos;S</h1>
+    </>
+  )
+}
+
+export function Main() {
+  return (
+    <>
+      <h2>Bodies</h2>
+      <p>Let the bodies hit the floor</p>
+    </>
+  )
+}
+
+export function Footer() {
+  return (
+    <>
+      <pre>Nothing is wrong with me</pre>
+    </>
+  )
+}
